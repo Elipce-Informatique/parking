d4c5a48b65ae3d4422423953b61169cd4c0dcd42
Yann Plantevin <yann.pltv@gmail.com>
2014-11-10 23:04:29 +0100

    10/11/2014 Update gulpfile - Déplacement des js libs dans assets (Bootstrap et jQuery) - Ajout d'un système de notification à gulp - Ajout de Stylus à Gulp - Création d'une tache JS - Installation de uglify js - Utilisation de uglify js dans la tache JS - Switch des jquery et bootstrap .js non mini dans le dossier assets - Passage des paths en variables de config (gulpfile) - Utilisation de gulp-changed pour détecter les fichiers modifiés entre 2 passages

--- changelog.md
+++ changelog.md
@@ -1,3 +1,14 @@
+###### 10/11/2014 Update gulpfile
+- Déplacement des js libs dans assets (Bootstrap et jQuery)
+- Ajout d'un système de notification à gulp
+- Ajout de Stylus à Gulp
+- Création d'une tache JS
+- Installation de uglify js
+- Utilisation de uglify js dans la tache JS
+- Switch des jquery et bootstrap .js non mini dans le dossier assets
+- Passage des paths en variables de config (gulpfile)
+- Utilisation de gulp-changed pour détecter les fichiers modifiés entre 2 passages
+
 ###### 10/11/2014 Mise en place css/js et gulp
 - Création des dossiers pour CSS et JS sous app/assets
 - Création d'une tache gulp CSS qui envoi le tout sous public/css
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,48 @@
-// Gulp requirement
+// GULP REQUIREMENT
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
-// Plugins requirements
+// PLUGINS REQUIREMENTS
+var changed = require('gulp-changed');
 var minifycss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var stylus = require('gulp-stylus');
+var uglify = require('gulp-uglify');
+
+// PATHS
+var CSS_SRC = 'app/assets/css/**/*.css';
+var CSS_DEST = 'public/css';
+var JS_SRC = 'app/assets/js/**/*.js';
+var JS_DEST = 'public/js';
 
 // Manipulation des CSS
 gulp.task('css', function () {
-    return gulp.src('app/assets/css/**/*.css')
+    return gulp.src(CSS_SRC)
+        .pipe(changed(CSS_DEST))
+        .pipe(stylus())
         .pipe(autoprefixer('> 1%'))
         .pipe(minifycss())
-        .pipe(gulp.dest('public/css'));
+        .pipe(gulp.dest(CSS_DEST))
+        .pipe(notify({message: 'CSS task completed.'}));
+});
+
+// Manipulation des JS
+gulp.task('js', function () {
+    return gulp.src(JS_SRC)
+        .pipe(changed(JS_DEST))
+        .pipe(uglify())
+        .pipe(gulp.dest(JS_DEST))
+        .pipe(notify({message: 'JS task completed.'}));
 });
 
 // Setup des watchers
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     // Watch des CSS
-    return gulp.watch('app/assets/css/**/*.css', ['css']);
+    gulp.watch(CSS_SRC, ['css']);
+    // Watch des js
+    gulp.watch(JS_SRC, ['js']);
 });
-
 
 /**
  * Tâche par défaut appellée quand on tape la commande 'gulp'
@@ -28,4 +51,4 @@ gulp.task('watch', function(){
  * - Lance les watchers pour les taches suivantes:
  *      - CSS
  */
-gulp.task('default', ['css', 'watch']);
+gulp.task('default', ['css', 'js', 'watch']);
