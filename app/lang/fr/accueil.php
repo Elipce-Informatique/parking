dc3206c5bb27e8b4afef0c7c2e9092029b68388f
Yann Plantevin <yann.pltv@gmail.com>
2014-12-02 08:54:07 +0100

    commit page accueil en cours

---
+++ app/lang/fr/accueil.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | TEXTES DE LA PAGE D'ACCUEIL
+    |--------------------------------------------------------------------------
+    */
+
+    'bienvenue_titre' => 'Bienvenue sur l\'application TEST!',
+    'bienvenue_text' => 'L\'application de test vous permet de gérer ce pourquoi elle a été développée. Vous pouvez accéder à plus d\'informations en cliquand sur le bouton bleu',
+    'bouton' => 'Bouton bleu'
+
+
+);
--- app/views/pages/accueil.blade.php
+++ app/views/pages/accueil.blade.php
@@ -5,25 +5,25 @@
 @stop
 
 @section('jumbotron')
-    <h1>Hello, world!</h1>
-    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+    <h1>{{Lang::get('accueil.bienvenue_titre')}}</h1>
+    <p>{{Lang::get('accueil.bienvenue_text')}}</p>
     <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
 @stop
 
 @section('content')
     <div class="row">
         <div class="col-md-4">
-          <h2>{{Lang::get('menu.top.item_1')}}</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <h2>{{Lang::get('menu.top.administration')}}</h2>
+          <p>Section d'administration de l'application. Ici les administrateurs peuvent gérer les utilisateurs, les profils, les droits d'accès aux différents modules.</p>
           <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
         </div>
         <div class="col-md-4">
-          <h2>{{Lang::get('menu.top.item_2')}}</h2>
+          <h2>Section 2</h2>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
           <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
        </div>
         <div class="col-md-4">
-          <h2>{{Lang::get('menu.top.item_3')}}</h2>
+          <h2>Section 3</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
           <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
         </div>
