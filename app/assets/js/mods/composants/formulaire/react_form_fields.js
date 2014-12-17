23ba508753027366561524ec467d00c80f00cb5d
Yann Plantevin <yann.pltv@gmail.com>
2014-12-17 15:34:18 +0100

    Refactor input vivians Création store gest mod voir #3

--- app/assets/js/mods/composants/formulaire/react_form_fields.js
+++ app/assets/js/mods/composants/formulaire/react_form_fields.js
@@ -1,4 +1,15 @@
+var Input = require('./react_input');
+
 module.exports = {
     Form: require('./react_form'),
-    Input: require('./react_input')
-}
+    InputText : Input.InputText,
+    InputTextEditable : Input.InputTextEditable,
+    InputMail : Input.InputMail,
+    InputMailEditable : Input.InputMailEditable,
+    InputPassword : Input.InputPassword,
+    InputPasswordEditable : Input.InputPasswordEditable,
+    InputRadio : Input.InputRadio,
+    InputRadioEditable : Input.InputRadioEditable,
+    InputCheckbox : Input.InputCheckbox,
+    InputCheckboxEditable : Input.InputCheckboxEditable
+};
---
+++ app/assets/js/mods/mixins/gestion_modif.js
@@ -0,0 +1,17 @@
+/**
+ * Created by yann on 17/12/2014.
+ */
+var mixinGestionModif = {
+    componentDidMount: function () {
+        this.attachEventsOnForm();
+    },
+    componentDidUpdate: function (pp, ps) {
+        this.attachEventsOnForm();
+    },
+    attachEventsOnForm: function () {
+        //console.log(this.getDOMNode());
+        var $form = $(this.getDOMNode());
+        $form.off(Actions.global.gestion_modif_change, '[data-gest-mod]');
+    }
+}
+module.exports = mixinGestionModif;
