ede5ccb6c656dc4088936c0e129c8de7080f8276
Pierre <pierre.armand@elipce.com>
2015-01-09 16:46:18 +0100

    Commit etat utilisateur

--- app/assets/js/utilisateur_courant.app.js
+++ app/assets/js/utilisateur_courant.app.js
@@ -1,9 +1,5 @@
 // ATTENTION la majuscule est super importante
-var PageUserCourant = require('./mods/page/react_page_utilisateur_courant');
 
 $(function(){
-    var oReactPageUserCourant = React.render(
-        <PageUserCourant />,
-        document.getElementById('content_user')
-    );
+
 });
