8552e217fdf8bcdfa4ebf53a61620322e7a57641
vivian-perez <vivian.perez@elipce.com>
2015-04-21 18:01:06 +0200

    simulator en cours de debug, presque pret

--- app/controllers/SimulatorController.php
+++ app/controllers/SimulatorController.php
@@ -37,17 +37,68 @@ class SimulatorController extends \BaseController
 
 
     /**
-     * SLibére ou occupe des places de parking
+     * Libére ou occupe des places de parking sur le niveau $id
      *
-     * @param  int $id : ID parking
+     * @param  int $id : ID niveau
      * @return Response
      */
     public function show($id)
     {
-        //
-       $places = Parking::getPlaces($id);
+        $retour = true;
+        // Toutes les places du parking $id
+        $places = Niveau::getPlaces($id);
 
-        return $places;
+        // Min et max des places
+        $min = 0;
+        $max = Niveau::getMaxPlace($id);
+
+        // Nb aléatoire de places qui bougent
+        $nb = mt_rand(5, 15);
+
+//        return Place::getPlaceFromNum(58, 1);
+
+        try {
+            DB::beginTransaction();
+            Log::debug('nb places :'.$nb);
+            // Parcours des places à modifier
+            for ($i = 0; $i < $nb; $i++) {
+                // Place aléatoire
+                $numPlace = mt_rand($min, $max);
+                Log::debug('num place'. $numPlace);
+                // ID place
+                $idPlace = Place::getPlaceFromNum($numPlace, $id)->id;
+                Log::debug('id place'. $idPlace);
+                // Modification place
+                // TODO calculer l'état d'occupation
+                if (!Place::updatePlace($idPlace, array('etat_occupation_id' => 11))) {
+                    DB::rollBack();
+                    $retour = false;
+                }
+                // Journal
+                if (!JournalEquipementNiveau::createJournalPlace(array(
+                    'niveau_id' => $id,
+                    'place_id' => $idPlace,
+                    'etat_occupation_id' => null
+                ))
+                ) {
+                    DB::rollBack();
+                    $retour = false;
+                }
+            }
+            // Pas d'exception si on arrive ici
+            DB::commit();
+        } catch (Exception $e) {
+            // Erreur SQL, on log
+            Log::error('Rollback simulator !'.print_r($e,true));
+            DB::rollBack();
+            $retour = false;
+        }
+        if($retour){
+            dd('OK');
+        }
+        else{
+            dd('KO');
+        }
     }
 
 
@@ -57,7 +108,8 @@ class SimulatorController extends \BaseController
      * @param  int $id
      * @return Response
      */
-    public function edit($id)
+    public
+    function edit($id)
     {
         //
     }
