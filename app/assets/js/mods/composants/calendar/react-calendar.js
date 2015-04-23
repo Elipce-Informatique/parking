b0ce263718aaa2386c0a68354639150ccccbecec
vivian-perez <vivian.perez@elipce.com>
2015-04-23 15:19:48 +0200

    Suppression calendar local

--- app/assets/js/mods/composants/calendar/react-calendar.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  Calendar: require('./src/Calendar'),
-  Month: require('./src/Month'),
-  Week: require('./src/Week'),
-  Day: require('./src/Day'),
-  dateUtils: require('./src/dateUtils')
-};
