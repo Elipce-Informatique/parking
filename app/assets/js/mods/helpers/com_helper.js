c1807b8afa5f2129a2b97bfbd8328352dad9dc2f
Yann Plantevin <yann.pltv@gmail.com>
2015-08-20 10:23:34 +0200

    SUPERVISION - Séparation com_helper et supervition_helper

--- app/assets/js/mods/helpers/com_helper.js
+++ app/assets/js/mods/helpers/com_helper.js
@@ -5,13 +5,6 @@ var conf = require('../../config/config.js');
  *
  */
 module.exports.client = {
-    _timer: false,
-    _planId: 0,
-    _journalId: 0,
-    _parkingId: 0,
-    _journalAlerteId: 0,
-    _ajaxInstances: {},
-
     host: null,
     port: null,
     client: {},
@@ -75,7 +68,7 @@ module.exports.client = {
 
                             // 2 - Message has a messageType key
                             if (message.messageType) {
-                                switch(message.messageType){
+                                switch (message.messageType) {
                                     case 'init_parking_finished':
                                         console.log('ACTION init_parking_finished');
                                         Actions.com.init_parking_finished();
@@ -96,119 +89,16 @@ module.exports.client = {
         } else {
             console.log('On a deja un websocket !!');
         }
-    },
-
-
-
-    /**
-     * Save parameters as data and try to connect to server
-     * @param planId
-     * @param journalId
-     * @param parkingId
-     * @param journalAlerteId
-     */
-    listenSupervision: function (planId, journalId, parkingId, journalAlerteId, onConnexion, onError) {
-        // INIT DATA
-        this._planId = planId;
-        this._journalId = journalId;
-        this._parkingId = parkingId;
-        this._journalAlerteId = journalAlerteId;
-
-        //// MODE REEL
-        Actions.supervision.parking_event.listen(this._handleAjax.bind(this));
-        this.initWebSocket(this._parkingId, onConnexion, onError);
-    },
-
-    destroyTimerPlaces: function () {
-        if (this._timer) {
-            clearInterval(this._timer);
-            this._timer = false;
-        }
-    },
-
-    /**
-     * Récupère les informations sur les places et déclenche l'action
-     * @private
-     */
-    _handleAjax: function () {
-        // 1 - UPDATE TABLEAU DE BORD EN PARALLÈLE
-        this.abortAjax();
-        Actions.supervision.tableau_bord_update(this._parkingId);
-
-        // --------------------------------------
-        // 2 - UPDATE PLAN ET JOURNAL SIDEBAR
-        this._ajaxInstances['1'] = $.ajax({
-            type: 'GET',
-            url: BASE_URI + 'parking/journal_place/' + this._planId + '/' + this._journalId,
-            context: this,
-            global: false
-        })
-            .done(function (data) {
-                // ON A DES NOUVELLES DONNÉES
-                if (data.length) {
-                    // 2.1 - UPDATE PLACES CARTE
-                    Actions.map.refresh_places(data, this._planId);
-                    // 2.2 - UPDATE JOURNAL TEMPS REEL
-                    Actions.supervision.temps_reel_update_journal(data);
-
-                    // Calcul du prochain nouvel ID journal
-                    _.each(data, function (p, i) {
-                        this._journalId = Math.max(this._journalId, p.latest_journal_equipement.id);
-                    }, this);
-                }
-            })
-            .fail(function (xhr, type, exception) {
-                // if ajax fails display error alert
-                console.error("ajax error response error " + type);
-                console.error("ajax error response body " + xhr.responseText);
-            });
-
-        // --------------------------------------------------------
-        // 3 - UPDATE ALERTES SIDEBAR
-        this._ajaxInstances['2'] = $.ajax({
-            type: 'get',
-            url: BASE_URI + 'parking/journal_alerte/' + this._planId + '/' + this._journalAlerteId,
-            processdata: false,
-            contenttype: false,
-            data: {},
-            context: this,
-            global: false
-        })
-            .done(function (data) {
-                if (data.length) {
-                    Actions.supervision.temps_reel_update_alertes(data);
-
-                    // Calcul du prochain nouvel ID journal
-                    _.each(data, function (p, i) {
-                        this._journalAlerteId = Math.max(this._journalAlerteId, p.id);
-                    }, this);
-                }
-            })
-            .fail(function (xhr, type, exception) {
-                // if ajax fails display error alert
-                console.error("ajax error response error " + type);
-                console.error("ajax error response body " + xhr.responsetext);
-            });
-    },
-
-    /**
-     * Annule toutes les requêtes ajax en cours
-     */
-    abortAjax: function () {
-        _.each(this._ajaxInstances, function ($req) {
-            $req.abort();
-        });
     }
-}
-;
+
+};
 
 var messages = ({
-
     /***************************************************************
      *************** CONTROLLER CONFIGURATION ************************
      ***************************************************************/
 
-    initParking: function(){
+    initParking: function () {
         return {
             messageType: 'init_parking',
             data: {}
@@ -242,7 +132,7 @@ var messages = ({
                 timezone: "Europe/Berlin",
                 syslogServer: syslogServer
             }
-        }
+        };
         return retour;
     },
 
