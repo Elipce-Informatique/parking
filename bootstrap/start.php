2197d6957eac31af63844839ad9da0872990e848
vivian-perez <vivian.perez@elipce.com>
2015-10-08 18:52:30 +0200

    Test prod

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('elipce-leaderpark'),
+	'production' => array('elipce-leaderpark'),
 
 ));
 
